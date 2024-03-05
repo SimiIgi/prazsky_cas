a1fa8086386b2dd5746dfc96ae46ee82ad6bc70e
“Simona <simona.ignacz@gmail.com>
2024-03-05 19:35:00 +0100

    zadanie_c_2

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,24 +2,42 @@ import React, { useState, useEffect } from 'react';
 
 const HomePage = () => {
   const [currentTime, setCurrentTime] = useState('');
+  const [timezone, setTimezone] = useState('Europe/Prague');
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://worldtimeapi.org/api/timezone/Europe/Prague');
+    
+      const response = await fetch(`https://worldtimeapi.org/api/timezone/${timezone}`);
       const data = await response.json();
+
 
       setCurrentTime(data.utc_datetime);
     };
 
     fetchData();
-  }, []);
+  }, [timezone]);
+
+  const handleTimeZoneChange = (event) => {
+  
+    setTimezone(event.target.value);
+  };
 
   return (
     <div>
-      <p>Hodnota datetime: {currentTime}</p>
+      <p>Hodnota datetime: {timezone}: {currentTime}</p>
+      
+      <label>Vyberte zónu: </label>
+      <select value={timezone} onChange={handleTimeZoneChange}>
+        <option value="America/New_York">New York</option>
+        <option value="Europe/London">Londýn</option>
+        <option value="Europe/Moscow">Moskva</option>
+        <option value="Europe/Prague">Praha</option>
+        <option value="Asia/Hong_Kong">Hong Kong</option>
+        <option value="Asia/Jerusalem">Jeruzalém</option>
+      </select>
+
     </div>
   );
 };
-
 
 export default HomePage;
